f1a68063ba34cbc193a9412001624fac73699eda
Melasa <apisarek2008@gmail.com>
2017-12-08 07:21:34 +0100

    Subject interface

--- DesignPattern/src/observer/WeatherData.java
+++ DesignPattern/src/observer/WeatherData.java
@@ -1,10 +1,48 @@
 package observer;
 
+import java.util.Arrays;
+
 public class WeatherData {
+	private float temp;
+	private float humid;
+	private float press;
+	Arrays Observers;
+	
 
 	public static void main(String[] args) {
-		System.out.println("Test...");
+		
 
+	}
+	
+	public void registerObserver() {
+		
+	}
+	
+	public void notifyObserver() {
+		
+	}
+	
+	public void removeObserver() {
+		
+	}
+
+	public void measurementChange() {
+		
+	}
+
+	public float getTemperature() {
+		this.temp = temp;
+		return temp;
+	}
+	
+	public float getHumidity() {
+		this.humid = humid;
+		return humid;
+	}
+	
+	public float getPressure() {
+		this.press = press;
+		return press
 	}
 
 }
